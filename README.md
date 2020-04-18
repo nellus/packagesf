28f3acd5cc3c70fb94f1458757be36fa5df88085
simone <martinellosimone01@gmail.com>
2020-04-18 21:34:13 +0200

    updated

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 npm package to create a package.xml file for Salesforce
 
 To create the package.xml file, import the module, and then run the function createPackage().
-This will create a package.xml file in the folder when you ran the command.
+This will create a package.xml file in the folder where you ran the command.
--- app.js
+++ app.js
@@ -13,19 +13,25 @@ exports.createPackage = () => {
 		metadatatypes.push(row);
   })
   .on('end', () => {
-    console.log('CSV file successfully processed');
-	});
-	
-	console.log('Writing package.xml for SF');
-	fs.open('package.xml', 'w', (err, fd) => {      
-		writeMyData(metadatatypes);
+		console.log('CSV file successfully processed');
+		console.log('Writing package.xml for SF');
+		fs.open('package.xml', 'w', (err, fd) => {      
+			writeMyData(metadatatypes);
+		});
 	});
 
 	function writeMyData(metadata){
-		let filestr = '';
+		let filestr = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\r\n';
+		filestr += '<Package xmlns="http://soap.sforce.com/2006/04/metadata">\r\n';
+		console.log('metadata ' , metadata);
 		metadata.forEach(m => {
-			filestr += '<'+m.Metadata+'></'+m.Metadata+'>';
+			filestr += '\t<types>\r\n';
+			filestr += '\t\t<members>*</members>\r\n';
+			filestr += '\t\t<name>'+m.Metadata+'</name>\r\n';
+			filestr += '\t</types>';
 		});
+
+		filestr += '\r\n</Package>';
 		console.log('filestr ' + filestr);
 		const data = new Uint8Array(Buffer.from(filestr));
 		fs.writeFile('package.xml', data, (err) => {
